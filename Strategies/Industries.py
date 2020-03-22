afed4f93818b0bbf7795381da2daebeaecf737e0
qhyhjl <qhyhjl@163.com>
2020-03-22 18:47:41 +0800

    initial file

--- Strategies/Industries.py
+++ Strategies/Industries.py
@@ -5,7 +5,6 @@ import pandas as pd
 import datetime
 import pymysql
 from sqlalchemy import create_engine
-import numpy as np
 
 token = '9585083c3cb3cc48af1ac4401d6be82cf738b23f1eeb31ce284011e5'
 ts.set_token(token)

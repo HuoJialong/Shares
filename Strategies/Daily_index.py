95081f02052550944422e8a63af275fd1ba42ee4
qhyhjl <qhyhjl@163.com>
2020-03-27 23:08:56 +0800

    almost finished

--- Strategies/Daily_index.py
+++ Strategies/Daily_index.py
@@ -6,7 +6,10 @@ token = '9585083c3cb3cc48af1ac4401d6be82cf738b23f1eeb31ce284011e5'
 ts.set_token(token)
 pro = ts.pro_api()
 
-days = 30
+day = 20
+index2 = pro.index_classify(level='L2', src='SW')
+# index2 = index2[~index2['industry_name'].isin(['动物保健', '园区开发', '石油开采', '水务', '渔业'])]
+index2 = index2[index2['industry_name'] == '石油开采']
 
 
 def trade_date_list():
@@ -17,6 +20,34 @@ def trade_date_list():
     return trade_dates
 
 
-trade_date = trade_date_list()
-stock = pro.daily_basic(ts_code='', begin_date=trade_date[days],
-                        fields='ts_code, trade_date, close, free_share')
+def daily_index(index2, day=0):
+    trade_date = trade_date_list()
+    stock0 = pro.daily_basic(ts_code='', trade_date=trade_date[day],
+                             fields='ts_code, trade_date, close, free_share')
+
+    index_info = pd.DataFrame()
+    for i in range(index2.shape[0]):
+        member = pro.index_member(index_code=index2['index_code'].values[i],
+                                  fields='index_code,con_code')
+        member.columns = ['index_code', 'ts_code']
+        index_info = index_info.append(member)
+
+    stock1 = pd.merge(stock0, index_info, how='left')
+    stock1 = pd.merge(stock1, index2[['index_code', 'industry_name']], how='left')
+    stock1 = stock1.dropna(axis=0, how='any')  # 删除含有NaN的行
+
+    result = pd.DataFrame()
+    for i in range(index2.shape[0]):
+        temp = stock1[stock1['index_code'] == index2['index_code'].values[i]]
+        sum_market = sum(temp['close']*temp['free_share'])
+        sum_free_share = temp['free_share'].sum()
+        temp = temp[['index_code', 'industry_name', 'trade_date']][0:1]
+        temp['point'] = sum_market/sum_free_share
+        result = result.append(temp)
+    return result
+
+
+index_point = pd.DataFrame()
+for j in range(day):
+    temp = daily_index(index2=index2, day=j)
+    index_point = index_point.append(temp)

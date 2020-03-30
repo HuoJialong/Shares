c70eadc934eadf7d1fc0ae6ad882fc3ae5716a49
qhyhjl <qhyhjl@163.com>
2020-03-30 22:33:25 +0800

    updated

--- Strategies/Daily_index.py
+++ Strategies/Daily_index.py
@@ -8,7 +8,7 @@ token = '9585083c3cb3cc48af1ac4401d6be82cf738b23f1eeb31ce284011e5'
 ts.set_token(token)
 pro = ts.pro_api()
 
-day = 30
+day = 0
 write = 1
 index2 = pro.index_classify(level='L2', src='SW')
 industry_list = [

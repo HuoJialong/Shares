3af3aca9574907b2970946da2c1110ab172542dd
qhyhjl <qhyhjl@163.com>
2020-04-02 21:05:17 +0800

    sql statement

---
+++ Strategies/industry_daily.sql
@@ -0,0 +1,6 @@
+-- select avg(total_mv), max(total_mv), min(total_mv), std(total_mv) from `industries`.`电子零部件制造`
+-- select * from stock.relative_strength
+SELECT 
+    *
+FROM
+    stock.industry_daily
